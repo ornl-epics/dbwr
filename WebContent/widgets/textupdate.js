5741f7ad0fe2728b5482761c80f55a5753cfc1b8
ky9 <ky9@cooper.ornl.gov>
2019-03-25 14:43:40 -0400

    Use data()

--- WebContent/widgets/textupdate.js
+++ WebContent/widgets/textupdate.js
@@ -44,7 +44,7 @@ function format_pv_data_as_text(widget, data)
     let text;
     if (data.text !== undefined)
     {
-        if (data.value !== undefined  &&  widget.attr("data-format") == "decimal")
+        if (data.value !== undefined  &&  widget.data("format") == "decimal")
             text = data.value.toString();
         else
             text = data.text;
@@ -55,21 +55,21 @@ function format_pv_data_as_text(widget, data)
             text = data.value
         else
         {
-            if (widget.attr("data-format") == "exponential")
+            if (widget.data("format") == "exponential")
             {
                 if (data.precision === undefined)
                     text = data.value.toExponential().replace('e', 'E');
                 else
                     text = data.value.toExponential(data.precision).replace('e', 'E');
             }
-            else if (widget.attr("data-format") == "engineering")
+            else if (widget.data("format") == "engineering")
                 text = format_engineering(data.value, data.precision);
-            else if (widget.attr("data-format") == "hex")
+            else if (widget.data("format") == "hex")
             {
                 text = (data.value | 0).toString(16);
                 text = "0x" + text;
             }
-            else if (widget.attr("data-format") == "binary")
+            else if (widget.data("format") == "binary")
             {
                 text = (data.value | 0).toString(2);
                 text = "0b" + text;
@@ -82,7 +82,7 @@ function format_pv_data_as_text(widget, data)
                     text = data.value.toFixed(data.precision);
             }
         }
-        if (data.units !== undefined  &&  widget.attr("data-show-units") != "false")
+        if (data.units !== undefined  &&  widget.data("show-units") != "false")
            text = text + " " + data.units;
     }
     else
