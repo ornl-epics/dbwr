929ca9a53e5c8c50a973dbe4aaf78737982551e1
ky9 <ky9@cooper.ornl.gov>
2019-07-17 17:46:07 -0400

    Text entry widget: Use '<input>', start write support

--- WebContent/widgets/textentry.js
+++ WebContent/widgets/textentry.js
@@ -1,1 +1,56 @@
-DisplayBuilderWebRuntime.prototype.widget_update_methods["textentry"] = update_text_widget
+
+DisplayBuilderWebRuntime.prototype.widget_init_methods['textentry'] = function(widget)
+{
+    widget.focusin(() =>
+    {
+        console.log("Focus in...." + widget.val());
+        widget.data("editing", widget.val());
+        
+    });
+
+    widget.focusout(() =>
+    {
+        console.log("Focus out..." + widget.val());
+        widget.val(widget.data("editing"))
+        widget.removeData("editing");
+    });
+    
+    widget.keydown(event =>
+    {
+        if (event.keyCode == 13)
+        {
+            let val = widget.val();
+            console.log("ENTERED " + widget.data("pv") + " = " + val);
+            // Loose focus, which restores the original value resp. most recent update
+            widget.blur();
+            console.log("For now back to " + widget.val());
+            // TODO write val to PV
+        }
+    })
+}
+
+
+DisplayBuilderWebRuntime.prototype.widget_update_methods['textentry'] = function(widget, data)
+{
+    let text = format_pv_data_as_text(widget, data);
+
+    if (widget.data("editing") !== undefined)
+    {
+        widget.data("editing", text);
+        console.log("Editing, caching " + text);
+        return;
+    }
+    
+    widget.val(text);
+    
+    // See textupdate.js to center vertically?
+
+    // TODO After testing the 'write' and 'return' vs. 'tab/escape' handling:
+    // widget.prop('disabled', data.readonly);
+    
+    if (data.readonly)
+        widget.css("cursor", "not-allowed");
+    else
+        widget.css("cursor", "auto");
+}
+
--- src/main/java/dbwr/widgets/TextEntryWidget.java
+++ src/main/java/dbwr/widgets/TextEntryWidget.java
@@ -6,6 +6,8 @@
  ******************************************************************************/
 package dbwr.widgets;
 
+import java.io.PrintWriter;
+
 import org.w3c.dom.Element;
 
 import dbwr.parser.WidgetFactory;
@@ -14,12 +16,31 @@ public class TextEntryWidget extends BaseTextWidget
 {
     static
     {
-        WidgetFactory.addJavaScript("textentry.js");
         WidgetFactory.registerLegacy("org.csstudio.opibuilder.widgets.TextInput", "textentry");
+        WidgetFactory.addJavaScript("textentry.js");
+        WidgetFactory.addCSS("textentry.css");
     }
 
 	public TextEntryWidget(final ParentWidget parent, final Element xml) throws Exception
 	{
 		super(parent, xml, "textentry", "#7FF");
+
+        attributes.put("type", "text");
+        attributes.put("value", "<" + pv_name + ">");
+
 	}
+
+    @Override
+    protected String getHTMLElement()
+    {
+        return "input";
+    }
+
+    @Override
+    protected void fillHTML(final PrintWriter html, final int indent)
+    {
+//        html.append("<input type=\"text\" value=\"");
+//        HTMLUtil.escape(html, "<" + pv_name + ">");
+//        html.append("\"/>");
+    }
 }
