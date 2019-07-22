511d3063e2f80eb8aeb0c35ae068b33c777cc088
ky9 <ky9@cooper.ornl.gov>
2019-07-22 10:02:28 -0400

    Picture 'file:' warning

--- WebContent/widgets/picture.js
+++ WebContent/widgets/picture.js
@@ -3,9 +3,14 @@ DisplayBuilderWebRuntime.prototype.widget_init_methods['picture'] = function(wid
 {
     let file = widget.data("file");
 
-    let link = dbwr.display + "/../" + file;
+    let link = dbwr.display;
+    let end = dbwr.display.lastIndexOf('/');
+    link = link.substring(0, end) + "/" + file;
     // Better, but unclear how widely this is supported:
     // let link = new URL(file, dbwr.display).href;
+
+    if (link.startsWith("file:"))
+        console.warn("Cannot load picture from " + link + " because web browser cannot access files on server.");
     
     // console.log("Loading picture " + link);
     widget.attr("src", link);
