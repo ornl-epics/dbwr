aca0b0cae8fc443aaa829667b17e77e0d1a81ed4
ky9 <ky9@cooper.ornl.gov>
2020-01-22 11:22:07 -0500

    Add rotation to Picture Widget
    Support BOY permutation_matrix as per Davy Dequidt as well as display
    builder rotation
    
    #1

--- src/main/java/dbwr/widgets/PictureWidget.java
+++ src/main/java/dbwr/widgets/PictureWidget.java
@@ -9,6 +9,7 @@ package dbwr.widgets;
 import java.io.PrintWriter;
 
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 import dbwr.parser.WidgetFactory;
 import dbwr.parser.XMLUtil;
@@ -32,9 +33,45 @@ public class PictureWidget extends Widget
 		final String file = XMLUtil.getChildString(this, xml, "file")
 		                           .orElse(XMLUtil.getChildString(this, xml, "image_file")
 		                                          .orElse("missing_image.png"));
+
+
 		attributes.put("data-file", file);
 
-		// See label for rotation?
+        // Rotation
+		final int rotate = XMLUtil.getChildDouble(xml, "rotation").orElse(0.0).intValue();
+		if (rotate != 0)
+		{
+		    // Plain rotation around center
+		    // For multiples of 90 degree, this matches the desktop display builder.
+		    // For other angles, the desktop version adjusts the image size to stay
+		    // within original bounds, which is omitted here.
+		    styles.put("transform", "rotate(" + rotate + "deg)");
+		}
+
+		final Element matrix = XMLUtil.getChildElement(xml, "permutation_matrix");
+		if (matrix != null)
+		{
+	        // Legacy (BOY):
+	        // <permutation_matrix>
+	        //   <row>
+	        //     <col>0.0</col>
+	        //     <col>-1.0</col>
+	        //   </row>
+	        //   <row>
+	        //     <col>1.0</col>
+	        //     <col>0.0</col>
+	        //   </row>
+	        // </permutation_matrix>
+	        // --> transform: matrix(0,1,-1,0,0,0);
+            final NodeList rows = matrix.getChildNodes();
+            final NodeList row1 = rows.item(1).getChildNodes();
+            final NodeList row2 = rows.item(3).getChildNodes();
+            if (row1.getLength() == 5 && row2.getLength() == 5)
+                styles.put("transform",
+                           "matrix(" + row2.item(3).getTextContent() + "," + row2.item(1).getTextContent() + ","
+                                + row1.item(3).getTextContent() + "," + row1.item(1).getTextContent()
+                                + ",1,1)");
+		}
 	}
 
 	@Override
