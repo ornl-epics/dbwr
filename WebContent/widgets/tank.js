650aad0795836670bf0518cfdba8bb3b087d6a2e
Dequidt Davy <dequidd@4504DS-SRV-0023.codac.iter.org>
2020-01-22 08:46:20 +0000

    Add tank widget

---
+++ WebContent/widgets/tank.js
@@ -0,0 +1,12 @@
+DisplayBuilderWebRuntime.prototype.widget_update_methods["tank"] = function (widget, data)
+{
+    //console.log(widget, data);
+    var tankbar = widget.find('.tankbar'),
+		inity = parseInt(tankbar.attr('inity')),
+		maxheight = parseInt(tankbar.attr('maxheight')),
+    	barsize = maxheight * data.value / data.max
+    widget.find('.tankbar').attr({
+    	'height': barsize,
+    	'y': inity + maxheight - barsize
+    });
+}
