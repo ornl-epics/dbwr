8ae21d5bac2b402d0aa062bbf6745834b9eed2ef
ky9 <ky9@cooper.ornl.gov>
2020-01-23 10:28:42 -0500

    spaces instead of tabs

--- src/main/java/dbwr/widgets/TabsWidget.java
+++ src/main/java/dbwr/widgets/TabsWidget.java
@@ -33,56 +33,56 @@ public class TabsWidget extends Widget
     private final List<String> labels = new ArrayList<>();
     private final List<List<Widget>> tabs = new ArrayList<>();
 
-	public TabsWidget(final ParentWidget parent, final Element xml) throws Exception
-	{
-		super(parent, xml, "tabs");
+    public TabsWidget(final ParentWidget parent, final Element xml) throws Exception
+    {
+        super(parent, xml, "tabs");
 
-		// classes.add("Debug");
+        // classes.add("Debug");
 
-		final FontInfo font = XMLUtil.getFont(xml, "font").orElse(LabelWidget.DEFAULT_FONT);
-		font.addToStyles(styles);
+        final FontInfo font = XMLUtil.getFont(xml, "font").orElse(LabelWidget.DEFAULT_FONT);
+        font.addToStyles(styles);
 
-		active = XMLUtil.getChildInteger(xml, "active_tab").orElse(0);
+        active = XMLUtil.getChildInteger(xml, "active_tab").orElse(0);
 
-		// Locate labels and content of tabs
-		final Element tbs = XMLUtil.getChildElement(xml, "tabs");
-		if (tbs != null)
-		{
-		    int i=0;
-		    for (final Element tb : XMLUtil.getChildElements(tbs, "tab"))
-		    {
-		        final String label = XMLUtil.getChildString(parent, tb, "name").orElse("Tab " + (i+1));
-		        labels.add(label);
+        // Locate labels and content of tabs
+        final Element tbs = XMLUtil.getChildElement(xml, "tabs");
+        if (tbs != null)
+        {
+            int i=0;
+            for (final Element tb : XMLUtil.getChildElements(tbs, "tab"))
+            {
+                final String label = XMLUtil.getChildString(parent, tb, "name").orElse("Tab " + (i+1));
+                labels.add(label);
 
-		        final List<Widget> tab = new ArrayList<>();
-		        final Element children = XMLUtil.getChildElement(tb, "children");
-		        if (children != null)
-		            for (final Element widget_xml : XMLUtil.getChildElements(children, "widget"))
-		                tab.add(WidgetFactory.createWidget(this, widget_xml));
-		        tabs.add(tab);
+                final List<Widget> tab = new ArrayList<>();
+                final Element children = XMLUtil.getChildElement(tb, "children");
+                if (children != null)
+                    for (final Element widget_xml : XMLUtil.getChildElements(children, "widget"))
+                        tab.add(WidgetFactory.createWidget(this, widget_xml));
+                tabs.add(tab);
 
-		        ++i;
-		    }
-		}
-		else
-		{
-		    // BOY used a 'tab_count' and then the matching number of .._title etc.
-		    // ITER has generated displays that have more .._title.. entries,
-		    // so limit to tab_count
-	        final int tab_count = XMLUtil.getChildInteger(xml, "tab_count").orElse(Integer.MAX_VALUE);
+                ++i;
+            }
+        }
+        else
+        {
+            // BOY used a 'tab_count' and then the matching number of .._title etc.
+            // ITER has generated displays that have more .._title.. entries,
+            // so limit to tab_count
+            final int tab_count = XMLUtil.getChildInteger(xml, "tab_count").orElse(Integer.MAX_VALUE);
 
-		    // Look for legacy *.opi "tab_0_title", "tab_1_title" etc.
-		    int i=0;
-		    String label = XMLUtil.getChildString(parent, xml, "tab_" + i + "_title").orElse(null);
-		    while (label != null)
-		    {
-		        labels.add(label);
-		        if (++i >= tab_count)
-		            break;
-		        label = XMLUtil.getChildString(parent, xml, "tab_" + i + "_title").orElse(null);
-		    }
-		    // Each tab's content was in a group <widget>'s children
-		    i = 0;
+            // Look for legacy *.opi "tab_0_title", "tab_1_title" etc.
+            int i=0;
+            String label = XMLUtil.getChildString(parent, xml, "tab_" + i + "_title").orElse(null);
+            while (label != null)
+            {
+                labels.add(label);
+                if (++i >= tab_count)
+                    break;
+                label = XMLUtil.getChildString(parent, xml, "tab_" + i + "_title").orElse(null);
+            }
+            // Each tab's content was in a group <widget>'s children
+            i = 0;
             for (final Element bodies : XMLUtil.getChildElements(xml, "widget"))
             {
                 final List<Widget> tab = new ArrayList<>();
@@ -92,16 +92,16 @@ public class TabsWidget extends Widget
                 if (++i >= tab_count)
                     break;
             }
-		}
-	}
+        }
+    }
 
-	@Override
-	protected void fillHTML(final PrintWriter html, final int indent)
-	{
+    @Override
+    protected void fillHTML(final PrintWriter html, final int indent)
+    {
         html.append("\n");
 
         // Tab headers
-	    HTMLUtil.indent(html, indent+1);
+        HTMLUtil.indent(html, indent+1);
         html.append("<ul class=\"Tabs\">\n");
         int i=0;
         for (final String label : labels)
@@ -141,6 +141,6 @@ public class TabsWidget extends Widget
             ++i;
         }
 
-	    HTMLUtil.indent(html, indent);
-	}
+        HTMLUtil.indent(html, indent);
+    }
 }
