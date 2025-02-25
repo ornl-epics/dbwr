faeba9d48ca9f257c950dcf835a0791819dfaaf8
Maria A. <mapsacosta@gmail.com>
2025-02-25 11:44:24 -0600

    Adding build files from dbwr-phoebus-webapp (#1)
    * Adding build files from dbwr-phoebus-webapp
    
    * Removing git and vim binaries
    
    * GitHub Actions: Update default branch
    
    ---------
    
    Co-authored-by: Mariana González Velarde <marianaglezv@gmail.com>

--- docker/setenv.sh
+++ docker/setenv.sh
@@ -8,3 +8,11 @@
 #export PVWS_HOST=localhost:8081
 #export PVWS_WS_PROTOCOL=ws
 #export PVWS_HTTP_PROTOCOL=http
+#
+
+export EPICS_CA_AUTO_ADDR_LIST= NO
+export EPICS_CA_ADDR_LIST=`cat /usr/local/epics/Config/EPICS_ADDR_LIST | xargs echo`
+export EPICS_PVA_AUTO_ADDR_LIST=NO
+export EPICS_PVA_ADDR_LIST=`cat /usr/local/epics/Config/EPICS_ADDR_LIST | xargs echo`
+export WHITELIST1=file:/displays/CSS/.*
+#export PV_DEFAULT_TYPE=pva
