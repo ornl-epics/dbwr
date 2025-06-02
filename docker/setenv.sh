e99418ffaa6b2a54a437358b21d84a32e3e2e93e
Mariana González Velarde <mariana@fnal.gov>
2025-06-02 11:26:46 -0500

    Restore git repo epicsENV source
    Signed-off-by: Mariana González Velarde <mariana@fnal.gov>

--- docker/setenv.sh
+++ docker/setenv.sh
@@ -6,9 +6,4 @@ export PV_DEFAULT_TYPE=pva
 export EPICS_HOST_INTERFACE='enp65s0f0'
 export EPICS_PVA_ADDR_LIST="239.128.1.6,8@$EPICS_HOST_INTERFACE 239.128.1.6"
 
-# Uncomment to restore NFS/Git repo epicsENV source
-# This cannot be done until https://ghe-pip2.fnal.gov/epics-controls/Config/issues/508 is solved
-#source /usr/local/epics/Config/epicsENV
-
-# Uncomment to use local, modified epicsENV source from configMap
-source /usr/local/dbwr/epicsENV
+source /usr/local/epics/Config/epicsENV
